06639f29a62ecef5cfb95fff7e09152c7142e4e9
BJ-HOME <webdevlcn@gmail.com>
2023-06-24 23:00:22 +0800

    字体加载器

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Icon from './icon.jpg'
 function component() {
   const element = document.createElement('div')
   // lodash 在当前 script 中使用 import 引入
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.innerHTML = _.join(['Hello', 'webpack', '为中华之崛起而读书'], ' ')
   element.classList.add('hello')
 
   const myIcon = new Image()
