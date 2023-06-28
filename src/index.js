4cb280a06ab9f5f07996c4864fd8aa07f3bc2fb7
BJ-HOME <webdevlcn@gmail.com>
2023-06-28 17:10:32 +0800

    htmlwebpackplugin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import _ from 'lodash'
+import printMe from './print.js'
 
 function component() {
   const element = document.createElement('div')
+  const btn = document.createElement('button')
+
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack', '为中华之崛起而读书'], ' ')
-
+  btn.innerHTML = 'Click me and check the console!'
+  btn.onclick = printMe
+  element.appendChild(btn)
   return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: { index: './src/index.js', print: './src/print.js' },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出'
+    })
+  ]
 }
